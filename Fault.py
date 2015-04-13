- Laplacian in TriangularTents is correct - gpsverticals can plot data, synth, transformation and res - remove_euler_rotation in gpsrates can estimate the rotation on a subset of station - Removed condition on the need to compute east and north when computing synthetic gps displacements for verticals

Fault.py
@@ -1718,9 +1718,9 @@ class Fault(SourceInv):
                 iS = np.sqrt(1./S)
                 D = D*iS[:,np.newaxis]
 
-            # Cm
-            D2 = np.dot(D.T, D)
-            localCm = 1./lam[i]*np.linalg.pinv(D2)
+            # LocalCm
+            D2 = np.dot(D.T,D)
+            localCm = 1./lam[i]*np.linalg.inv(D2)
 
             # Put it into Cm
             Cm[ist:ied, ist:ied] = localCm

TriangularTents.py
@@ -725,8 +725,8 @@ class TriangularTents(TriangularPatches):
         Args:
             * verbose       : if True, displays stuff.
             * method        : Method to estimate the Laplacian operator
-                'count'     --> The diagonal is the number of surrounding nodes.
-                                Off diagonals are -1 for the surrounding nodes, 0 otherwise.
+                'count'     --> The diagonal is 2-times the number of surrounding nodes.
+                                Off diagonals are -2/(number of surrounding nodes) for the surrounding nodes, 0 otherwise.
 
                 'distance'  --> Computes the scale-dependent operator based on Desbrun et al 1999.
                                 
@@ -744,28 +744,46 @@ class TriangularTents(TriangularPatches):
         # Allocate an array
         D = np.zeros((len(vertices), len(vertices)))
 
-        # Iterate over the vertices
-        for adja, i in zip(self.adjacentTents, range(len(vertices))):
-            
-            if method=='count':
-
-                D[i,i] = float(len(adja))
-                D[i,adja] = -1.
-
-            elif method=='distance':
-
+        # Normalize the distances
+        if method=='distance':
+            self.Distances = []
+            for adja, i in zip(self.adjacentTents, range(len(vertices))):
                 x0, y0, z0 = vertices[i,0], vertices[i,1], vertices[i,2]
-                xv, yv, zv = vertices[adja,0], vertices[adja,1], vertices[adja,2]
-                distances = np.array([np.sqrt((x-x0)**2+(y-y0)**2+(z-z0)**2) for x, y, z in zip(xv, yv, zv)])
+                xv, yv, zv = vertices[adja,0], vertices[adja,1], vertices[adja,2] 
+                distances = np.array([np.sqrt((x-x0)**2+(y-y0)**2+(z-z0)**2) 
+                    for x, y, z in zip(xv, yv, zv)])
+                self.Distances.append(distances)
+            normalizer = np.max([d.max() for d in self.Distances])
+
+        # Iterate over the vertices
+        i = 0
+        for adja in self.adjacentTents:
+            # Counting Laplacian
+            if method=='count':
+                D[i,i] = 2*float(len(adja))
+                D[i,adja] = -2./float(len(adja))
+            # Distance-based
+            elif method=='distance':
+                distances = self.Distances[i]/normalizer
                 E = np.sum(distances)
-                D[i,i] = 2./E * np.sum(1./distances)
+                D[i,i] = float(len(adja))*2./E * np.sum(1./distances)
                 D[i,adja] = -2./E * 1./distances
 
+            # Increment 
+            i += 1
+
         # Condition
-        if np.linalg.eigvals(D).min() < 0.:
-            S, V = np.linalg.eig(D)
-            S[S<0.] = 0.
-            D = np.dot(np.dot(V.T, np.diag(S)), V)
+        #S, V = np.linalg.eig(D) 
+        #uu = 0
+        #while S.min()<0.:
+        #    uu += 1
+        #    S[S<0.] = 0.
+        #    D = np.dot(np.dot(V.T, np.diag(S)), V)
+        #    S, V = np.linalg.eig(D)
+        #    if uu>100:
+        #        print('Laplacian cannot be conditioned easily.')
+        #        print('You will have to think about something smart...')
+        #        return D
 
         # All done
         return D

geodeticplot.py
@@ -743,24 +743,61 @@ class geodeticplot(object):
 
         # Plot the name of the stations if asked
         if name:
+            font = {'family' : 'serif',
+                    'color'  : 'k',
+                    'weight' : 'normal',
+                    'size'   : 15}
             for lo, la, sta in zip(lon.tolist(), lat.tolist(), gps.station):
-                self.carte.ax.text(lo, la, sta, fontsize=12)
+                self.carte.ax.text(lo, la, sta, fontdict=font)
 
         # All done
         return
 
-    def gpsverticals(self, gps, norm=None, colorbar=True, markersize=20.):
+    def gpsverticals(self, gps, norm=None, colorbar=True, data=['data'], markersize=[10], linewidth=0.1):
         '''
         Scatter plot of the vertical displacement of the GPS.
         '''
 
-        # Get xy
-        x = gps.lon
-        x[x<0.] += 360.
-        y = gps.lat
+        # Assert
+        if (type(data) is not list) and (type(data) is str):
+            data = [data]
+        if (type(markersize) is not list):
+            markersize = [markersize]
+        if len(markersize)==1 and len(data)>1:
+            markersize = [markersize[0] for d in data]
 
-        # Get the verticals
-        V = gps.vel_enu[:,2]
+        # Get lon lat
+        lon = gps.lon
+        lat = gps.lat
+        lon[lon<0.] += 360.
+
+        # Initiate
+        vmin = 999999999.
+        vmax = -999999999.
+
+        # Make the dictionary of the things to plot
+        Data = {}
+        for dtype,mark in zip(data, markersize):
+            if dtype is 'data':
+                dName = '{} Data'.format(gps.name)
+                Values = gps.vel_enu[:,2]
+            elif dtype is 'synth':
+                dName = '{} Synth.'.format(gps.name)
+                Values = gps.synth[:,2]
+            elif dtype is 'res':
+                dName = '{} Res.'.format(gps.name)
+                Values = gps.vel_enu[:,2] - gps.synth[:,2] 
+            elif dtype is 'strain':
+                dName = '{} Strain'.format(gps.name)
+                Values = gps.Strain[:,2]
+            elif dtype is 'transformation':
+                dName = '{} Trans.'.format(gps.name)
+                Values = gps.transformation[:,2]
+            Data[dName] = {}
+            Data[dName]['Values'] = Values
+            Data[dName]['Markersize'] = mark
+            vmin = np.min([vmin, np.min(Values)])
+            vmax = np.max([vmax, np.max(Values)])
 
         # Get a colormap
         cmap = plt.get_cmap('jet')
@@ -769,12 +806,12 @@ class geodeticplot(object):
         if norm is not None:
             vmin = norm[0]
             vmax = norm[1]
-        else:
-            vmin = V.min()
-            vmax = V.max()
 
         # Plot that on the map
-        sc = self.carte.scatter(x, y, s=markersize, c=V, cmap=cmap, vmin=vmin, vmax=vmax, linewidth=0.01)
+        for dName in Data:
+            mark = Data[dName]['Markersize']
+            V = Data[dName]['Values']
+            sc = self.carte.scatter(lon, lat, s=mark, c=V, cmap=cmap, vmin=vmin, vmax=vmax, linewidth=linewidth)
 
         # Colorbar
         if colorbar:

gpsrates.py
@@ -1682,19 +1682,42 @@ class gpsrates(SourceInv):
         # All done
         return
 
-    def remove_euler_rotation(self, eradius=6378137.0):
+    def remove_euler_rotation(self, eradius=6378137.0, stations=None, verbose=True):
         '''
         Removes the best fit Euler rotation from the network.
         Args:
             * eradius   : Radius of the earth (should not change that much :-)).
+            * stations  : List of stations on which rotation is estimated. If None, 
+                          uses all the stations.
         '''
-
-        print ("Remove the best fot euler rotation in GPS data set {}".format(self.name))
+        if verbose:
+            print('--------------------------------------------------')
+            print('--------------------------------------------------')
+            print ("Remove the best fot euler rotation in GPS data set {}".format(self.name))
 
         import eulerPoleUtils as eu
 
+        # Get the list of stations
+        if stations is not None:
+            lon = []
+            lat = []
+            vel = []
+            for station in stations:
+                u = np.flatnonzero(self.station==station)
+                if len(u)>0:
+                    lon.append(self.lon[u[0]]*np.pi/180.)
+                    lat.append(self.lat[u[0]]*np.pi/180.)
+                    vel.append(self.vel_enu[u[0],:2]/self.factor)
+            lon = np.array(lon)
+            lat = np.array(lat)
+            vel = np.array(vel)
+        else:
+            lon = self.lon*np.pi/180.
+            lat = self.lat*np.pi/180.
+            vel = self.vel_enu[:,:2]/self.factor
+
         # Estimate the roation pole coordinates and the velocity
-        self.elat,self.elon,self.omega = eu.gps2euler(self.lat*np.pi/180., self.lon*np.pi/180., np.zeros(self.lon.shape), self.vel_enu[:,0]/self.factor, self.vel_enu[:,1]/self.factor)
+        self.elat,self.elon,self.omega = eu.gps2euler(lat, lon, np.zeros(lon.shape), vel[:,0], vel[:,1])
 
         # Remove the rotation
         self.remove_rotation(self.elon, self.elat, self.omega)
@@ -1825,8 +1848,8 @@ class gpsrates(SourceInv):
                     self.synth[:,1] += ss_synth[N:N+Nd]
                     N += Nd
                 if vertical:
-                    if ss_synth.size > 2*Nd and east and north:
-                        self.synth[:,2] += ss_synth[N:N+Nd]
+                    # if ss_synth.size > 2*Nd and east and north:
+                    self.synth[:,2] += ss_synth[N:N+Nd]
             if ('d' in direction) and ('dipslip' in G.keys()):
                 Gd = G['dipslip']
                 Sd = fault.slip[:,1]
@@ -1839,8 +1862,8 @@ class gpsrates(SourceInv):
                     self.synth[:,1] += ds_synth[N:N+Nd]
                     N += Nd
                 if vertical:
-                    if ds_synth.size > 2*Nd and east and north:
-                        self.synth[:,2] += ds_synth[N:N+Nd]
+                    #if ds_synth.size > 2*Nd and east and north:
+                    self.synth[:,2] += ds_synth[N:N+Nd]
             if ('t' in direction) and ('tensile' in G.keys()):
                 Gt = G['tensile']
                 St = fault.slip[:,2]
@@ -1853,22 +1876,22 @@ class gpsrates(SourceInv):
                     self.synth[:,1] += op_synth[N:N+Nd]
                     N += Nd
                 if vertical:
-                    if op_synth.size > 2*Nd and east and north:
-                        self.synth[:,2] += op_synth[N:N+Nd]
+                    #if op_synth.size > 2*Nd and east and north:
+                    self.synth[:,2] += op_synth[N:N+Nd]
             if ('c' in direction) and ('coupling' in G.keys()):
                 Gc = G['coupling']
                 Sc = fault.coupling
                 dc_synth = np.dot(Gc,Sc)
                 N = 0
                 if east:
-                    self.synth[:,0] += dc_synth[0:Nd]
+                    self.synth[:,0] += dc_synth[N:Nd]
                     N += Nd
                 if north:
                     self.synth[:,1] += dc_synth[N:N+Nd]
                     N += Nd
                 if vertical:
-                    if dc_synth.size > 2*Nd and east and north:
-                        self.synth[:,2] += dc_synth[N:N+Nd]
+                    #if dc_synth.size > 2*Nd and east and north:
+                    self.synth[:,2] += dc_synth[N:N+Nd]
 
             if poly == 'build' or poly == 'include':
                 if (self.name in fault.poly.keys()):