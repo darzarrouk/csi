0a93db3525cea8c97897e1c52c2fa1d503d34a2f
Romain Jolivet <jolivetinsar@gmail.com>
2015-04-15 17:49:08 +0100

    - Added a getSubNetwork function in gpsrates

--- Fault.py
+++ Fault.py
@@ -2009,6 +2009,58 @@ class Fault(SourceInv):
         # All done
         return
 
+    def writePatchesCenters2File(self, filename, add_slip=None, scale=1.0):
+        '''
+        Writes the patch corners in a file that can be used in psxyz.
+        Args:
+            * filename      : Name of the file.
+            * add_slip      : Put the slip as a value for the color.
+                              Can be None, strikeslip, dipslip, total, coupling
+            * scale         : Multiply the slip value by a factor.
+            * patch         : Can be 'normal' or 'equiv'
+        '''
+
+        # Check size
+        if self.N_slip!=None and self.N_slip!=len(self.patch):
+            raise NotImplementedError('Only works for len(slip)==len(patch)')
+
+        # Write something
+        print('Writing geometry to file {}'.format(filename))
+
+        # Open the file
+        fout = open(filename, 'w')
+
+        # Loop over the patches
+        nPatches = len(self.patch)
+        for patch in self.patch:
+
+            # Get patch index
+            pIndex = self.getindex(patch)
+
+            # Get patch center
+            xc, yc, zc = self.getcenter(patch)
+            lonc, latc = self.xy2ll(xc, yc)
+
+            # Select the string for the color
+            if add_slip is not None:
+                if add_slip is 'coupling':
+                    slp = self.coupling[pIndex]
+                elif add_slip is 'strikeslip':
+                    slp = self.slip[pIndex,0]*scale
+                elif add_slip is 'dipslip':
+                    slp = self.slip[pIndex,1]*scale
+                elif add_slip is 'total':
+                    slp = np.sqrt(self.slip[pIndex,0]**2 + self.slip[pIndex,1]**2)*scale
+
+            # Write the string to file
+            fout.write('{} {} {} {} \n'.format(lonc, latc, zc, slp))
+
+        # Close the file
+        fout.close()
+
+        # All done
+        return
+
     def sumPatches(self, iPatches, finalPatch):
         '''
         Takes a list of patches, sums the green's functions,
