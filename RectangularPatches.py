Debugs dip calculation in RectangularPatches, Fixes Incorrect indentations.

RectangularPatches.py
@@ -1372,7 +1372,7 @@ class RectangularPatches(Fault):
             strike+= 2*np.pi
 
         # Set the dip
-        dip = np.arcsin(vd/width )
+        dip = np.arcsin(vd[2]/width )
 
         # All done
         return x1, x2, x3, width, length, strike, dip
@@ -2394,7 +2394,7 @@ class RectangularPatches(Fault):
         lonmin = np.min([p[:,0] for p in self.patchll])-expand
 	
         if lonmin<0: 
-		    lonmin += 360
+            lonmin += 360
         lonmax = np.max([p[:,0] for p in self.patchll])+expand
         if lonmax<0:
             lonmax+= 360

TriangularTents.py
@@ -1198,7 +1198,7 @@ class TriangularTents(TriangularPatches):
         lonmin = np.min([p[:,0] for p in self.patchll])-expand
 	
         if lonmin<0: 
-		    lonmin += 360
+            lonmin += 360
         lonmax = np.max([p[:,0] for p in self.patchll])+expand
         if lonmax<0:
             lonmax+= 360

opticorr.py
@@ -648,43 +648,43 @@ class opticorr(SourceInv):
         # Get number of points
         nd = self.east.shape[0]
 
-	if ptype >=3: 
+        if ptype >=3: 
 		# Compute normalizing factors
-		x0 = self.x[0]
-		y0 = self.y[0]
-		normX = np.abs(self.x - x0).max()
-		normY = np.abs(self.y - y0).max()
+                x0 = self.x[0]
+                y0 = self.y[0]
+                normX = np.abs(self.x - x0).max()
+                normY = np.abs(self.y - y0).max()
 
-		# Save them for later
-		self.OrbNormalizingFactor = {}
-		self.OrbNormalizingFactor['ref'] = [x0, y0]
-		self.OrbNormalizingFactor['x'] = normX
-		self.OrbNormalizingFactor['y'] = normY
+                # Save them for later
+                self.OrbNormalizingFactor = {}
+                self.OrbNormalizingFactor['ref'] = [x0, y0]
+                self.OrbNormalizingFactor['x'] = normX
+                self.OrbNormalizingFactor['y'] = normY
 
-		# Pre-compute position-dependent functional forms
-		f1 = self.factor * np.ones((nd,))
-		f2 = self.factor * (self.x - x0) / normX
-		f3 = self.factor * (self.y - y0) / normY
-		f4 = self.factor * (self.x - x0) * (self.y - y0) / (normX*normY)
-		f5 = self.factor * (self.x - x0)**2 / normX**2
-		f6 = self.factor * (self.y - y0)**2 / normY**2
-        	polyFuncs = [f1, f2, f3, f4, f5, f6]
+                # Pre-compute position-dependent functional forms
+                f1 = self.factor * np.ones((nd,))
+                f2 = self.factor * (self.x - x0) / normX
+                f3 = self.factor * (self.y - y0) / normY
+                f4 = self.factor * (self.x - x0) * (self.y - y0) / (normX*normY)
+                f5 = self.factor * (self.x - x0)**2 / normX**2
+                f6 = self.factor * (self.y - y0)**2 / normY**2
+                polyFuncs = [f1, f2, f3, f4, f5, f6]
 
- 	       # Fill in orb matrix given an order
-		orb = np.zeros((nd, basePoly))
-		for ind in range(basePoly):
-		    orb[:,ind] = polyFuncs[ind]
+                # Fill in orb matrix given an order
+                orb = np.zeros((nd, basePoly))
+                for ind in range(basePoly):
+                    orb[:,ind] = polyFuncs[ind]
 
-		# Block diagonal for both components
-		orb = block_diag(orb, orb)
+                # Block diagonal for both components
+                orb = block_diag(orb, orb)
 
-		# Check to see if we're including verticals
-		if self.obs_per_station == 3:
-		    orb = np.vstack((orb, np.zeros((nd, 2*basePoly))))
+                # Check to see if we're including verticals
+                if self.obs_per_station == 3:
+                    orb = np.vstack((orb, np.zeros((nd, 2*basePoly))))
 
-	elif basePoly==1:
-		orb = np.zeros((2*nd,1*basePoly))
-		orb[:,0] = self.factor	
+        elif basePoly==1:
+            orb = np.zeros((2*nd,1*basePoly))
+            orb[:,0] = self.factor	
 
 
         # All done
@@ -797,7 +797,7 @@ class opticorr(SourceInv):
         # Correct
         self.east -= self.east_synth
         self.north -= self.north_synth
-	
+
         # All done
         return
 