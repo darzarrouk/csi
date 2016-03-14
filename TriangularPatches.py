- TriangularPatches and TriangularTents can now handle vertical faults - Solve a small plot bug in TriangularPatches - Improve lecture of covariance file in imagecovariance.py

TriangularPatches.py
@@ -1055,12 +1055,19 @@ class TriangularPatches(Fault):
         # Get the patch normal
         normal = np.cross(p2 - p1, p3 - p1)
         normal /= np.linalg.norm(normal)
-
         # Enforce clockwise circulation
-        if normal[2] < 0:
+        if np.round(normal[2],decimals=1) < 0:
             normal *= -1.0
             p2, p3 = p3, p2
 
+        # If fault is vertical, force normal to be horizontal
+        if np.round(normal[2],decimals=1) == 0.: 
+            normal[2] = 0.
+        # Force strike between 0 and 90 or between 270 and 360
+            if normal[1] > 0:
+                normal *= -1
+             
+                    
         # Get the strike vector and strike angle
         strike = np.arctan2(-normal[0], normal[1]) - np.pi
         if strike<0.:
@@ -1830,9 +1837,9 @@ class TriangularPatches(Fault):
         '''
 
         # Get lons lats
-        lon = np.unique([p[:,0] for p in self.patchll])
+        lon = np.unique(np.array([p[:,0] for p in self.patchll]))
         lon[lon<0.] += 360.
-        lat = np.unique([p[:,1] for p in self.patchll])
+        lat = np.unique(np.array([p[:,1] for p in self.patchll]))
         lonmin = lon.min()-expand
         lonmax = lon.max()+expand
         latmin = lat.min()-expand

imagecovariance.py
@@ -647,7 +647,6 @@ class imagecovariance(object):
 
         '''
 
-        import re
         import linecache
 
         tmp = np.loadtxt(filename,comments='#')
@@ -657,9 +656,9 @@ class imagecovariance(object):
         l5 = linecache.getline(filename,5)
 
         self.datasets[dname]['function'] = 'exp'
-        self.datasets[dname]['Sill'] = float(re.findall('\d+.\d+',l3)[0])
-        self.datasets[dname]['Sigma'] = float(re.findall('\d+.\d+',l4)[0])
-        self.datasets[dname]['Lambda'] = float(re.findall('\d+.\d+',l5)[0])
+        self.datasets[dname]['Sill'] = float(l3.split()[-1])
+        self.datasets[dname]['Sigma'] = float(l4.split()[-1])
+        self.datasets[dname]['Lambda'] = float(l5.split()[-1])
         self.datasets[dname]['Distance'] = tmp[:,0]
         self.datasets[dname]['Semivariogram'] = tmp[:,1]
         self.datasets[dname]['Semivariogram Std'] = tmp[:,2]