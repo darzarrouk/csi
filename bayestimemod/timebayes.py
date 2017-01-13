3e46160f47eb50ebab6ffbc0ddab164e91670280
Zacharie Duputel <zacharie.duputel@unistra.fr>
2017-01-13 18:32:01 +0100

    Time Bayes to be tested

--- bayestimemod/timebayes.py
+++ bayestimemod/timebayes.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class timebayes(object):
 
-    def __init__(self, data, time, sigma, dt):
+    def __init__(self, data, time, sigma, dt, bounds, nsamples=1000):
         '''
         Initialization of a timebayes instance.
         This class solves the interpolation problem 
@@ -16,6 +16,8 @@ class timebayes(object):
             * time      : time vector
             * sigma     : noise (1 value, std)
             * dt        : delta-time between the interpolating functions
+            * bounds    : bounds of the uniform prior PDF
+            * nsamples  : number of samples
         '''
 
         # Init
@@ -23,6 +25,8 @@ class timebayes(object):
         self.time = time
         self.sigma = sigma
         self.dt = dt
+        self.bounds = bounds
+        self.nsamples = nsamples
         
         # Create the mpi framework
         import mpi4py
@@ -36,44 +40,61 @@ class timebayes(object):
         # All done
         return
 
-    def generateInitialSample(self, bounds, nSamples=1000):
+    def generateInitialSample(self):
         '''
         Generate the initial set of samples for the first step, 
         given a uniform prior (just samples the prior).
-
-        Args:
-            * bounds    : Bounds of the uniform pdf
-            * nSamples  : Number of samples to generate (default: 1000)
         '''
 
-        # Generate
-        if self.me == 0:
-            samples = np.random.rand(nSamples)*(bounds[1]-bounds[0]) - bounds[0]
-        else:
-            samples = None
+        return np.random.rand(self.nsamples)*(self.bounds[1]-self.bounds[0]) + self.bounds[0]
 
-        # Save the samples
-        self.samples = [samples]
 
-        # All done
-        return
-
-   def initializePredFunction(self):
+   def initializePredFunction(self,triTimes,times,h=None):
        '''
        Initialize the prediction function.
 
        Args:
-            * 
-            * 
+            * triTimes: triangle center times
+            * triTimes: observation times
         '''
 
         # Do stuff
-
+        def predict(alphas):
+            '''
+            Linear interpolation using triangular base functions
+            Args:
+            - triTimes: time at the center of each triangle
+            - alphas: amplitude of each triangle
+            - times: observation times
+            - h: triangle half-width (if not specified, we assume h=triTimes[1]-triTimes[0])
+            '''
+            # Triangle half-width
+            if h==None:
+                h = triTimes[1]-triTimes[0]
+            # Linear interpolation
+            p = np.zeros(times.shape)
+            for i in range(len(triTimes)):
+                dt = np.abs(times - triTimes[i])
+                j = np.where(dt<h)
+                p[j] += alphas[i] * (1-dt[j]/h)
+            # All done
+            return p
+        
         # Save the prediction function
-        self.fpred = predictionFunction
+        self.fpred = predict
 
         # All done
         return
+
+    def initializeTriangleMap(self):
+        '''
+        Initialize a map linking data time steps to the index of the preceeding triangle
+        '''
+        self.triangleMap = np.array(list(map(int,self.time/self.dt)))
+
+        # All done
+        return
+
 
     def oneTimeStep(self, step):
         '''
@@ -84,14 +105,33 @@ class timebayes(object):
         '''
 
         # Identify the data
-
+        triIndex  = self.triangleMap[step] # Index of the previous triangle
+        ti = triIndex*self.dt                     # Time of the previous triangle
+        di = np.where(np.logical_and(self.time>=ti-self.dt,self.time<=self.time[step]))
+        data = self.data[di]
+        time = self.time[di]
+        
         # Identify the samples to update/create and the fixed ones
-
-        fixed = 
-        samples = # un truc de dimension (2, nSamples)
-
+        fixed   = None
+        samples = None
+        if not self.me:
+            samples = np.zeros((self.nsamples,2))            
+            if step == 0:
+                fixed = np.zeros((self.nsamples,))
+                samples[:,0] = self.generateInitialSample()
+                samples[:,1] = self.generateInitialSample()
+            else:
+                fixed = self.samples[triIndex-1].copy()
+                samples[:,0] = self.samples[:,triIndex].copy()
+                if triIndex > self.triangleMap[step-1]:
+                    samples[:,1] = self.generateInitialSample()
+                else:
+                    samples[:,1] = self.samples[:,triIndex+1].copy()
+                
         # Create a prediction function
-
+        triTimes = np.arange(triIndex-1,triIndex+2)*self.dt        
+        self.initializePredFunction(triTimes,time,h=triTimes[1]-triTimes[0])
+        
         # Split the samples in as many workers 
         if self.me==0:
             
@@ -115,7 +155,7 @@ class timebayes(object):
         # Walk the chains in each worker
         sampler = resample(data, self.sigma, time, 
                            subsamples, subfixed, self.bounds, 
-                           prediction, self.comm)
+                           self.fpred, self.comm)
         subsamples = sampler.sample()
 
         # Send to master
@@ -128,18 +168,64 @@ class timebayes(object):
                 newsamples = self.comm.recv(source=worker, tag=worker+20)
                 alpha1 += newsamples[0]
                 alpha2 += newsamples[1]
-
+        else:
+            alpha1   = None
+            alpha2   = None
+                
         # All done
-        return alpha1, alpha2
+        return alpha1, alpha2, triIndex
 
     def walkWithTime(self):
         '''
         Advance throught time.
         '''
 
+        # Model sample set
+        self.samples = []
+
+        # Create data <-> triangle map
+        self.initializeTriangleMap()
+
         # Iterate over the data
-
-
+        for step in range(self.data.size):
+            # Walk one step
+            alpha1,alpha2, triIndex = self.oneTimeStep(step)
+            # Update sample set
+            if alpha1 is not None:
+                if triIndex>len(self.samples)-1:
+                    self.samples.append(alpha1)
+                else:
+                    self.samples[triIndex] = alpha1.copy()
+                if triIndex+1>len(self.samples)-1:
+                    self.samples.append(alpha2)
+                else:
+                    self.samples[triIndex+1] = alpha2.copy()
+        
+        # All done
+        return
+            
+    def plot(self):
+        '''
+        Plot data and results if there is some
+        '''
+        if not self.me:
+            # Triangle center times
+            triTimes = np.arange(self.samples.shape[1])*self.dt
+            # Initialize prediction function
+            self.initializePredFunction(triTimes,self.time,h=triTimes[1]-triTimes[0])
+            # Get stochastic predictions
+            preds = []
+            for i in range(self.samples.shape[0]):
+                preds.append(self.fpred(self.samples[i,:]))
+            # Plot them all
+            plt.plot(self.time,preds,'0.75')
+            plt.plot(self.time,self.data,'ko-')
+            plt.show()
+        # All done
+        return
+                
+        
+        
 # List splitter
 def _split_seq(seq, size):
     newseq = []
