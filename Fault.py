Fixed an issue with the straike averaging in TriangularTents

Fault.py
@@ -2516,10 +2516,6 @@ class Fault(SourceInv):
             azimuth += 360.
         
         # Get strikes and dips
-        #if self.patchType is 'triangletent':
-        #    strike = super(self.__class__, self).getStrikes()
-        #    dip = super(self.__class__, self).getDips()
-        #else:
         strike, dip = self.getStrikes(), self.getDips()
 
         # Convert angle in radians
@@ -2528,7 +2524,7 @@ class Fault(SourceInv):
                             np.cos(dip)*(1.+np.tan(azimuth)*np.tan(strike)))
 
         # If azimuth within ]90, 270], change rotation
-        if azimuth > 90. and azimuth<=270.:
+        if azimuth*180./np.pi > 90. and azimuth*180./np.pi<=270.:
             rotation += np.pi
 
         # Store rotation angles

TriangularTents.py
@@ -120,7 +120,9 @@ class TriangularTents(TriangularPatches):
             strike.append(stk)
             dip.append(dp)
 
-        strike = np.sum(np.array(strike) %(2*np.pi)) / len(strike)
+        # Compute the mean (beware of angle stuff)
+        j = np.complex(0., 1.)
+        strike = np.angle(np.sum([np.exp(j*s) for s in strike])/len(strike))
         dip = np.mean(dip)
 
         # All done

gps.py
@@ -2075,7 +2075,7 @@ class gps(SourceInv):
 
     def computeBestHelmert(self, components=2):
         '''
-        Fits a full Helmert transform to a network and remove it
+        Fits a full Helmert transform to the network 
 
         Args:
             * components    : Take the 2 horizontal (default) or 3 enu
@@ -2089,6 +2089,28 @@ class gps(SourceInv):
 
         # Run the estimation
         self.Helmert, res, rank, s = np.linalg.lstsq(Hf, d)
+
+        # All done
+        return
+
+    def removeBestHelmert(self, components=2):
+        '''
+        Fits a Helmert transform to the network and removes it.
+
+        Args:
+            * components    : Take the 2 horizontal (default) or 3 enu
+        '''
+
+        # Compute Helmert
+        self.computeBestHelmert(components=components)
+
+        # Get Matrix
+        H = self.getHelmertMatrix(components=components)
+
+        # Remove it
+        m = self.Helmert
+        self.vel_enu[:,:components] -= np.dot(H, m).reshape((components,
+                                                             self.vel_enu.shape[0])).T 
 
         # All done
         return