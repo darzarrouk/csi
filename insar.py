548296cd46b83056a412e7586243979575fe7f04
Baptiste Gombert <gombert@unistra.fr>
2018-01-02 15:30:41 +0100

    Fix error in poly estimator in Insar

--- insar.py
+++ insar.py
@@ -1078,7 +1078,7 @@ class insar(SourceInv):
         y0 = np.mean(self.y)
         normX = np.abs(self.x - x0).max()
         normY = np.abs(self.y - y0).max()
-        base_max = np.max([np.abs(base_x).max(), np.abs(base_y).max()])
+        base_max = np.max([normX, normY])
 
         self.TransformNormalizingFactor = {}
         self.TransformNormalizingFactor['x'] = normX
