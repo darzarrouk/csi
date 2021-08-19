-Added the alpha argument in plotting -Generalized the output of splitted patches in EDKSmp -Fixed an issue in Cm calculation in the case of different distances along strke and along dip

EDKSmp.py
@@ -311,21 +311,7 @@ def dropSourcesInPatches(fault, verbose=False, returnSplittedPatches=False):
 
     # All done
     if returnSplittedPatches:
-        from .TriangularPatches import TriangularPatches as trianglePatches
-        splitFault = trianglePatches('Splitted {}'.format(fault.name), 
-                                     utmzone=fault.utmzone, 
-                                     lon0=fault.lon0,
-                                     lat0=fault.lat0,
-                                     ellps=fault.ellps,
-                                     verbose=verbose)
-        # set up patches
-        splitFault.patch = [np.array(p) for p in allSplitted]
-        splitFault.patch2ll()
-        # Patches 2 vertices
-        splitFault.setVerticesFromPatches()
-        # Depth
-        splitFault.setdepth()
-        return Ids, Xs, Ys, Zs, Strikes, Dips, Areas, splitFault
+        return Ids, Xs, Ys, Zs, Strikes, Dips, Areas, allSplitted
     else:
         return Ids, Xs, Ys, Zs, Strikes, Dips, Areas
 # ----------------------------------------------------------------------

Fault.py
@@ -2438,7 +2438,7 @@ class Fault(SourceInv):
             lam0 = np.sqrt(xd**2 + yd**2 + zd**2)
         if verbose:
             print("Lambda0 = {}".format(lam0))
-        C = (sigma * lam0 / lam[0])**2 * (sigma * lam0 / lam[1])**2
+        C = (sigma * lam0 / lam[0]) * (sigma * lam0 / lam[1])
 
         # Creates the principal Cm matrix
         if self.N_slip==None:
@@ -2450,7 +2450,7 @@ class Fault(SourceInv):
 
         # Loop over the patches
         Hdistances,Vdistances = self.distancesMatrix(distance='center', lim=lim)
-        Cmt = C * np.exp(-Hdistances / lam[0]) * np.exp(-Vdistances / lam[1])
+        Cmt = C * np.exp(-0.5 * Hdistances / lam[0]) * np.exp(-0.5 * Vdistances / lam[1])
 
         # Store that into Cm
         st = 0

TriangularPatches.py
@@ -2843,14 +2843,38 @@ class TriangularPatches(Fault):
             * fault         : Returns a triangularpatches instance
         '''
 
+        # Check
+        if not hasattr(self, 'keepTrackOfSources'):
+            self.keepTrackOfSources = False
+
         # Import What is needed
         from .EDKSmp import dropSourcesInPatches as Patches2Sources
 
         # Drop the sources in the patches and get the corresponding fault
-        Ids, xs, ys, zs, strike, dip, Areas, fault = Patches2Sources(self, 
-                                                verbose=verbose,
-                                                returnSplittedPatches=True)
-        self.plotSources = [Ids, xs, ys, zs, strike, dip, Areas]
+        if not self.keepTrackOfSources:
+            Ids, xs, ys, zs, strike, dip, Areas, allSplitted = Patches2Sources(self, 
+                                                                    verbose=verbose,
+                                                          returnSplittedPatches=True)
+            self.plotSources = [Ids, xs, ys, zs, strike, dip, Areas]
+            self.allSplitted = allSplitted
+        else:
+            Ids, xs, ys, zs, strike, dip, Areas  = self.plotSources
+            allSplitted = self.allSplitted
+
+        # Create a new fault
+        fault = TriangularPatches('Splitted {}'.format(self.name), 
+                                  utmzone=self.utmzone, 
+                                  lon0=self.lon0,
+                                  lat0=self.lat0,
+                                  ellps=self.ellps,
+                                  verbose=verbose)
+        # set up patches
+        fault.patch = [np.array(p) for p in allSplitted]
+        fault.patch2ll()
+        # Patches 2 vertices
+        fault.setVerticesFromPatches()
+        # Depth
+        fault.setdepth()
 
         # Interpolate the slip on each subsource
         fault.initializeslip()

geodeticplot.py
@@ -92,6 +92,7 @@ class geodeticplot(object):
         gl = carte.gridlines(crs=self.projection, draw_labels=True, alpha=0.5)
         gl.xlabel_style = {'size': 'large', 'color': 'k', 'weight': 'bold'}
         gl.ylabel_style = {'size': 'large', 'color': 'k', 'weight': 'bold'}
+        self.cartegl = gl
         #carte.set_xticks(carte.get_xticks())
         #carte.set_yticks(carte.get_yticks())
         #carte.tick_params(axis='both', which='major', labelsize='large')
@@ -618,7 +619,7 @@ class geodeticplot(object):
     def faultpatches(self, fault, slip='strikeslip', norm=None, colorbar=True,
                      cbaxis=[0.1, 0.2, 0.1, 0.02], cborientation='horizontal', cblabel='',
                      plot_on_2d=False, revmap=False, linewidth=1.0, cmap='jet',
-                     transparency=0.0, factor=1.0, zorder=3, edgecolor='slip'):
+                     transparency=0.0, factor=1.0, zorder=3, edgecolor='slip', colorscale='normal'):
         '''
         Plot the fualt patches
 
@@ -640,6 +641,7 @@ class geodeticplot(object):
             * factor        : scale factor for fault slip values
             * zorder        : matplotlib order of plotting
             * edgecolor     : either a color or 'slip'
+            * colorscale    : 'normal' or 'log'
 
         Returns:
             * None
@@ -680,7 +682,10 @@ class geodeticplot(object):
             cmap = plt.get_cmap(cmap)
         else:
             cmap = plt.get_cmap(cmap)
-        cNorm  = colors.Normalize(vmin=vmin, vmax=vmax)
+        if colorscale in ('normal', 'n'):
+            cNorm  = colors.Normalize(vmin=vmin, vmax=vmax)
+        elif colorscale in ('log', 'l', 'lognormal'):
+            cNorm = colors.LogNorm(vmin=vmin, vmax=vmax)
         scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cmap)
 
         Xs = np.array([])
@@ -1078,7 +1083,7 @@ class geodeticplot(object):
         return
 
     def gps(self, gps, data=['data'], color=['k'], scale=None, scale_units=None, 
-            legendscale=10., linewidths=.1, name=False, zorder=5):
+            legendscale=10., linewidths=.1, name=False, zorder=5, alpha=1.):
         '''
         Args:
             * gps           : gps object from gps.
@@ -1146,7 +1151,7 @@ class geodeticplot(object):
                                   values[:,0], values[:,1],
                                   width=0.005, color=c,
                                   scale=scale, scale_units=scale_units,
-                                  linewidths=linewidths, zorder=zorder)
+                                  linewidths=linewidths, zorder=zorder, alpha=alpha)
             # TODO TODO TODO TODO TODO
             #if np.isfinite(self.err_enu[:,0]).all() and np.isfinite(self.err_enu[:,1]).all():
                 # Extract the location of the arrow head
@@ -1176,7 +1181,7 @@ class geodeticplot(object):
     def gpsverticals(self, gps, norm=None, colorbar=True,
                      cbaxis=[0.1, 0.2, 0.1, 0.02], cborientation='horizontal', cblabel='',
                      data=['data'], markersize=[10], linewidth=0.1,
-                     zorder=4, cmap='jet', marker='o'):
+                     zorder=4, cmap='jet', marker='o', alpha=1.):
         '''
         Scatter plot of the vertical displacement of the GPS.
 
@@ -1251,7 +1256,7 @@ class geodeticplot(object):
         for dName in Data:
             mark = Data[dName]['Markersize']
             V = Data[dName]['Values']
-            sc = self.carte.scatter(lon, lat, s=mark, c=V, cmap=cmap, vmin=vmin, vmax=vmax, linewidth=linewidth, zorder=zorder)
+            sc = self.carte.scatter(lon, lat, s=mark, c=V, cmap=cmap, vmin=vmin, vmax=vmax, linewidth=linewidth, zorder=zorder, alpha=alpha)
 
         # Colorbar
         if colorbar:
@@ -1261,7 +1266,7 @@ class geodeticplot(object):
 
     def gpsprojected(self, gps, norm=None, colorbar=True, 
                      cbaxis=[0.1, 0.2, 0.1, 0.02], cborientation='horizontal', cblabel='',
-                     zorder=4, cmap='jet'):
+                     zorder=4, cmap='jet', alpha=1.):
         '''
         Plot the gps data projected in the LOS
 
@@ -1296,7 +1301,7 @@ class geodeticplot(object):
         cmap = plt.get_cmap(cmap)
 
         # Plot
-        sc = self.carte.scatter(lon, lat, s=100, c=d, cmap=cmap, vmin=vmin, vmax=vmax, linewidth=0.5, zorder=zorder)
+        sc = self.carte.scatter(lon, lat, s=100, c=d, cmap=cmap, vmin=vmin, vmax=vmax, linewidth=0.5, zorder=zorder, alpha=alpha)
 
         # plot colorbar
         if colorbar:
@@ -1429,7 +1434,7 @@ class geodeticplot(object):
         return
 
     def insar(self, insar, norm=None, colorbar=True, 
-                    cbaxis=[0.1, 0.2, 0.1, 0.02], cborientation='horizontal', cblabel='',
+                    cbaxis=[0.2,0.2,0.1,0.01], cborientation='horizontal', cblabel='',
                     data='data', plotType='scatter', cmap='jet',
                     decim=1, zorder=3, edgewidth=1, alpha=1.):
         '''
@@ -1536,7 +1541,7 @@ class geodeticplot(object):
             if hasattr(insar, 'nx') and hasattr(insar, 'ny'):
                 lon = insar.lon.reshape((insar.ny,insar.nx))
                 lat = insar.lat.reshape((insar.ny,insar.nx))
-                data = insar.vel.reshape((insar.ny,insar.nx))
+                data = d.reshape((insar.ny,insar.nx))
             else:
                 # Build an interpolator
                 sarint = sciint.LinearNDInterpolator(np.vstack((x,y)).T, d, fill_value=np.nan)

insar.py
@@ -1997,7 +1997,7 @@ class insar(SourceInv):
 
         # Get average value
         if method=='mean':
-            reference = profile['LOS Velocity'][ii].mean()
+            reference = np.nanmean(profile['LOS Velocity'][ii])
         elif method=='linear':
             y = profile['LOS Velocity'][ii]
             x = profile['Distance'][ii]
@@ -2613,9 +2613,9 @@ class insar(SourceInv):
         y = self.profiles[name]['LOS Velocity']
         ey = self.profiles[name]['LOS Error']
         try:
-            p = prof.errorbar(x, y, yerr=ey, label='LOS', fmt='o')
+            p = prof.errorbar(x, y, yerr=ey, label='LOS', fmt='o', alpha=alpha)
         except:
-            p = prof.plot(x, y, label='LOS', marker='.')
+            p = prof.plot(x, y, label='LOS', marker='.', alpha=alpha)
         if synth:
             sy = self.profiles[name]['LOS Synthetics']
             s = prof.plot(x, sy, '-r', label='synthetics')

insartimeseries.py
@@ -369,7 +369,7 @@ class insartimeseries(insar):
 
     def readFromKFts(self, h5file, setmaster2zero=None,
                            zfile=None, lonfile=None, latfile=None, filetype='f',
-                           incidence=None, heading=None, inctype='onefloat', closeh5=True,
+                           incidence=None, heading=None, inctype='onefloat', closeh5=True, box=None,
                            field='rawts', error='rawts_std', keepnan=False, mask=None, readModel=False):
         '''
         Read the output from a typical GIAnT h5 output file.
@@ -384,6 +384,7 @@ class insartimeseries(insar):
             * latfile       : File with latitudes 
             * filetype      : type of data in lon, lat and elevation file (default: 'f')
             * incidence     : Incidence angle (degree)
+            * box           : Crop data (default None), ex: [y0:y1,x0:x1]
             * heading       : Heading angle (degree)
             * inctype       : Type of the incidence and heading values (see insar.py for details). Can be 'onefloat', 'grd', 'binary', 'binaryfloat'
             * field         : Name of the field in the h5 file.
@@ -402,10 +403,16 @@ class insartimeseries(insar):
         data = h5in[field]
         err = h5in[field+'_std']
 
+        # Box
+        if box is None:
+            bbox = [0,data.shape[0],0,data.shape[1]]
+        else:
+            bbox = box
+
         # Get some sizes
         nDates = data.shape[2]
-        nLines = data.shape[0]
-        nCols  = data.shape[1]
+        nLines = bbox[1]-bbox[0]
+        nCols = bbox[3]-bbox[2]
 
         # Deal with the mask instructions
         if mask is not None:
@@ -415,9 +422,9 @@ class insartimeseries(insar):
                 instruction = mask[2]
                 mask = np.ones((nLines, nCols))
                 if instruction in ('above'):
-                    mask[np.where(h5in[key][:]>value)] = np.nan
+                    mask[np.where(h5in[key][bbox[0]:bbox[1],bbox[2]:bbox[3]]>value)] = np.nan
                 elif instruction in ('under'):
-                    mask[np.where(h5in[key][:]<value)] = np.nan
+                    mask[np.where(h5in[key][bbox[0]:bbox[1],bbox[2]:bbox[3]]<value)] = np.nan
                 else:
                     print('Unknow instruction type for Masking...')
                     sys.exit(1)
@@ -426,9 +433,9 @@ class insartimeseries(insar):
 
         # Read Lon Lat
         if lonfile is not None:
-            self.lon = np.fromfile(lonfile, dtype=filetype)
+            self.lon = np.fromfile(lonfile, dtype=filetype).reshape((data.shape[0],data.shape[1]))[bbox[0]:bbox[1],bbox[2]:bbox[3]].flatten()
         if latfile is not None:
-            self.lat = np.fromfile(latfile, dtype=filetype)
+            self.lat = np.fromfile(latfile, dtype=filetype).reshape((data.shape[0],data.shape[1]))[bbox[0]:bbox[1],bbox[2]:bbox[3]].flatten()
 
         # Compute utm
         self.x, self.y = self.ll2xy(self.lon, self.lat) 
@@ -438,7 +445,8 @@ class insartimeseries(insar):
             self.elevation = insar('Elevation', utmzone=self.utmzone, 
                                    verbose=False, lon0=self.lon0, lat0=self.lat0,
                                    ellps=self.ellps)
-            self.elevation.read_from_binary(zfile, lonfile, latfile, 
+            z = np.fromfile(zfile, dtype=filetype).reshape((data.shape[0],data.shape[1]))[bbox[0]:bbox[1],bbox[2]:bbox[3]].flatten()
+            self.elevation.read_from_binary(z, self.lon, self.lat, 
                                             incidence=None, heading=None, 
                                             remove_nan=False, remove_zeros=False, 
                                             dtype=filetype)
@@ -458,8 +466,8 @@ class insartimeseries(insar):
             
             # Get things
             date = self.time[i]
-            dat = data[:,:,i]*mask
-            std = err[:,:,i]*mask
+            dat = data[bbox[0]:bbox[1],bbox[2]:bbox[3],i]*mask[bbox[0]:bbox[1],bbox[2]:bbox[3]]
+            std = err[bbox[0]:bbox[1],bbox[2]:bbox[3],i]*mask[bbox[0]:bbox[1],bbox[2]:bbox[3]]
 
             # check master date
             if i is setmaster2zero:
@@ -485,6 +493,7 @@ class insartimeseries(insar):
 
             # Take care of the LOS
             if incidence is not None and heading is not None:
+                assert box is None, 'Incidence cropping not implemented yet'
                 sar.inchd2los(incidence, heading, origin=inctype)
             else:
                 sar.los = np.zeros((sar.vel.shape[0], 3))
@@ -1451,7 +1460,7 @@ class insartimeseries(insar):
         return
 
     def plotProfiles(self, prefix, figure=124, show=True, norm=None, xlim=None, zlim=None, marker='.', color='k', line=False, linewidth=2, figsize=(20, 20), 
-            view=None, markersize=0.1):
+            view=None, markersize=0.1, aspectRatio=1.):
         '''
         Plots the profiles in 3D plot.
 
@@ -1469,6 +1478,7 @@ class insartimeseries(insar):
             * line          : If True, plots a line (default is False)
             * linewidth     : controls the width of the line
             * view          : list of elevation angle and azimuth (default is None)
+            * aspectRatio   : aspect of the z-axis with respect to other axis
 
         Returns:
             * None
@@ -1530,6 +1540,10 @@ class insartimeseries(insar):
             else:
                 ax.plot3D(nDate, distance, values, '-', 
                           color=color, linewidth=linewidth)
+
+        # Aspect
+        if aspectRatio>1. or aspectRatio<0.: aspectRatio=1.
+        ax.set_box_aspect((1.,1.,aspectRatio))
 
         # norm
         if norm is not None: